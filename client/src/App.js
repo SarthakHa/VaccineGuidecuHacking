f099fc08e2f5458c2b6606ff98182386aabfbf4e
Pak Shing Kan <kennethkankps@gmail.com>
2021-01-30 13:45:47 -0800

    updated the main page

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,14 @@ const App = ()=> {
     <div className="background">
        <div className="grid-container">
          <main className="main">
-         Guide Vaccine
+         <div className="guide-vaccine"> <h1>Guide</h1><h1>Vaccine</h1></div>
+         <div><h2>Find the optimal vaccine distribution across your own chosen places</h2></div>
+         <div className="choose-btn-div">  <button> <h3>Choose your Places!</h3></button> </div>
+        
          </main>
   
-     <div className="intro">
-       Find the optimal vaccine distribution across your own chosen places
-     </div>
-     <div className="choose-btn">
-       <button>Choose your Places!</button>
-     </div>
-    <div className="vabar">
+  
+    <div className="navbar">
       <h3>Home</h3>
       <h3>Selection</h3>
       <h3>The Model</h3>
@@ -24,6 +22,10 @@ const App = ()=> {
       
     </div>
 
+    <div className="image-div">
+    <img className="main-image" src="https://cdn.animaapp.com/projects/6015a5662fef2ded030dfd20/releases/6015af349a9b7cfb373827ee/img/ellipse-1@2x.svg"/>
+    </div>
+  
     </div>
     </div>
    
