e41d52f15fe6cfd96ef3a99a070f8e8dfb5a9307
Jehan <jehan.dastoor@mail.mcgill.ca>
2021-01-30 13:14:50 -0500

    defining all our functions

--- model_fitting/fitter.py
+++ model_fitting/fitter.py
@@ -1,3 +1,24 @@
 import numpy as np
 import covsirphy as cs
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TKAGG")
 
+class covsir_models:
+    def __init__(self,countries,states=None):
+        self.countries = countries
+        self.states = states
+        self.n = len(states) if states>0 else len(countries)
+    
+    #We use an SIR-D Model    
+    def model(self):
+        x = 0
+        
+    def retrieve_population(self):
+        x = 0
+    
+    def final_state(self):
+        x = 0
+        
+if __name__ == "__main__": #Just for testing
+    x = 0
