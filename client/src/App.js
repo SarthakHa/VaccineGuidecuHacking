a9675b90199d81e4361a684f56edcbc8b231236c
Pak Shing Kan <kennethkankps@gmail.com>
2021-01-30 12:36:33 -0800

    setting up the frontend layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React,{Fragment,useState,useEffect,createRef} from 'react'
+import './App.css';
 
-export default function App() {
+const App = ()=> {
+
   return (
-    <div>
-     bruh 
+    <div className="background">
+       <div className="grid-container">
+         <main className="main">
+         Guide Vaccine
+         </main>
+  
+     <div className="intro">
+       Find the optimal vaccine distribution across your own chosen places
+     </div>
+     <div className="choose-btn">
+       <button>Choose your Places!</button>
+     </div>
+    <div className="vabar">
+      <h3>Home</h3>
+      <h3>Selection</h3>
+      <h3>The Model</h3>
+      <h3>The Simulation</h3>
+      
     </div>
+
+    </div>
+    </div>
+   
   )
 }
+
+export default App;
