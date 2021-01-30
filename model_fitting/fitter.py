689284bac1683650e33c876bf7189a213d21c9e8
Jehan <jehan.dastoor@mail.mcgill.ca>
2021-01-30 15:25:33 -0500

    it works :)

--- model_fitting/fitter.py
+++ model_fitting/fitter.py
@@ -18,22 +18,23 @@ class covsir_models:
         params = {} #Storing the parameter values as a dictionary ("country" : [Parameters])
         for i in range(self.n):
             if len(self.countries) > 1:
-                snl = cs.Scenario(self.data,self.population_data,country=self.countries[i],province=None,tau=1440)
+                snl = cs.Scenario(self.jhu_data,self.population_data,country=self.countries[i],province=None,tau=1440)
             else:
-                snl = cs.Scenario(self.data,self.population_data,countries=self.countries[0],province=self.states[i],tau=1440)
-        snl.trend(show_figure=False)
-        snl.estimate(cs.SIRD,timeout=60)
-        if len(self.countries) > 1:
-            params[self.countries[i]] = pars
-        else:
-            params[self.states[i]] = [pars]
+                snl = cs.Scenario(self.jhu_data,self.population_data,countries=self.countries[0],province=self.states[i],tau=1440)
+            snl.trend(show_figure=False)
+            snl.estimate(cs.SIRD,timeout=60)
+            pars = [snl.get("rho","last"),snl.get("sigma","last"), snl.get("kappa","last")]
+            if len(self.countries) > 1:
+                params[self.countries[i]] = pars
+            else:
+                params[self.states[i]] = [pars]
         return params
         
     def retrieve_population(self):
         self.population = {} #Writing as a dictionary
         for i in range(self.n):
             if len(self.countries) > 1:
-                self.population[self.countryes[i]] = self.population_data.value(self.countries[i], province = None)
+                self.population[self.countries[i]] = self.population_data.value(self.countries[i], province = None)
             else:
                 self.population[self.states[i]] = self.population_data.value(self.countries[0], province = self.states[i])
         return self.population
@@ -41,7 +42,7 @@ class covsir_models:
     def final_state(self):
         state = {} #Writing as a dictionary
         for i in range(self.n):
-            if countries > 1:
+            if len(self.countries) > 1:
                 state[self.countries[i]] = self.jhu_data.records(self.countries[i])
             else: 
                 state[self.countries[i]] = self.jhu_data.records(self.countries[0],self.states[i])
