24bf3ea6aae62d69b3439328b2fafcc14f90aeaa
siddharthgir <siddharthgir>
2021-01-31 08:22:47 -0500

    Adding usage example

--- server/client_functions.py
+++ server/client_functions.py
@@ -1,4 +1,7 @@
 import urllib.request
+import numpy
+import io
+import time
 
 def send_request(countries,states,total_vaccines,efficacy,total_time,iterations,uid):
     string = "http://128.2.178.158:6968?uid={0}&total_time={1}&total_vaccines={2}&efficacy={3}&iterations={4}&countries=".format(uid,total_time,total_vaccines,efficacy,iterations)
@@ -14,15 +17,26 @@ def send_request(countries,states,total_vaccines,efficacy,total_time,iterations,
     print(string)
     with urllib.request.urlopen(string) as f:
         html = f.read().decode('utf-8')
-        print(html)
     return html
 
-def check_existence(file_name):
-    string = "http://128.2.178.158:4208/user_data/"+file_name
+def check_existence(uid,file_name):
+    string = "http://128.2.178.158:4208/user_data/"+str(uid)+"/"+file_name
     try:
         with urllib.request.urlopen(string) as f:
-                data = f.read().decode('utf-8')
-                return data
+                data = f.read()
+                formatted_data = numpy.load(io.BytesIO(data),allow_pickle=True)
+                
+                return formatted_data
     except:
-        return False
+        return []
 
+if __name__ == "__main__": #Just testing to see if it works, this won't run otherwise
+    port_no = send_request(["USA"],["Kentucky","Texas","Arizona"],100000,0.97,180,100,4)
+    print("link is ", "http://128.2.178.158:"+str(port_no))
+    while True:
+        time.sleep(5)
+        temp = check_existence(4,"policy_data.npy")
+        if len(temp) != 0:
+            print(temp)
+            break
+        print("couldn't find")
