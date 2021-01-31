e14b4e48c996ff010759702bad24cd192da81f30
Sarthak Ubuntu New Mac <sjsarthakh@gmail.com>
2021-01-31 06:22:57 +0000

    Modified.

--- server/vaccFunctions.py
+++ server/vaccFunctions.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 import ssl
+from client_functions.py import send_request
 from urllib.request import urlretrieve
 import tempfile
 import shutil
@@ -23,5 +24,27 @@ def testReq():
 def check():
     #UserID, Countries, States, numDays, Vaccine, Efficacy, numVaccs, numIterations
     req_data = request.get_json()
-    data = req_data["ID"]
-    return jsonify({"ID": data})
+    efficacy = 0
+    vaccine = req_data["Vaccine"]
+    vaccine = vaccine[0:3]
+    if vaccine == "Comi":
+        efficacy = 95.0
+    elif vaccine == "mRNA":
+        efficacy = 94.1
+    elif vaccine == "Coro":
+        efficacy = 78.0
+    elif vaccine == "AZD1":
+        efficacy = 70.4
+    elif vaccine == "Sput":
+        efficacy = 94.1
+    #elif vaccine == "BBIB":
+        #efficacy = 79.3
+    else:
+        return jsonify({"error": "Vaccine name not valid."})
+    if req_data["numDays"] < 1 or req_data["numDays"] > 180:
+        return jsonify({"error": "Number of days not valid."})
+    if req_data["numVaccs"] < 0 or req_data["numVaccs"] > 20000000:
+        return jsonify({"error": "Number of vaccines not valid."})
+    iterations = 1000 #Default value
+    html = send_request(req_data["Countries"], req_data["States"], req_data["numVaccs"], efficacy, req_data["numDays"], iterations, req_data["UserID"])
+    return jsonify(html)
