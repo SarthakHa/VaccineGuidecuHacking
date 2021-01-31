6ac098099124696d18791b8219e8b10c4ecbc97c
Jehan <jehan.dastoor@mail.mcgill.ca>
2021-01-31 02:39:02 -0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import Button from '@material-ui/core/Button'
 import './App.css';
 
 const App = ()=> {
-
   const [data,setData] = useState ([[1000,2000,3000,4000,5000],[3000,2000,1000,500,100],[5000,1000,2000,4000,5000]])
   const [contries,setContries]= useState( ["USA","UK","Canada", "Russia","South Africa"])
   const [readyToDisplay, setReadyToDisplay] = useState(false);
@@ -98,7 +97,20 @@ const App = ()=> {
             <h1>Learn About the Model</h1>
             
             <h3> The model we have used to fit and simulate the data is called an SIRD model. SIRD stands for Susceptible, Infected, Recovered and Deceased and is a standard epidemic model (note that N is the total population and is assumed to be unchanging). This model has been chosen due to its relatively quick optimization time when running and due to the generally poorly compiled COVID-19 data sets that do not contain the statistics necessary
-            for more complicated models. Mathematically, the SIRD model is represented by a system of ODEs: <span class="ellipsis">...</span> <span class="moreText">with the full text hidden</span> <a class="more" href="#">read more</a></h3>
+            for more complicated models. Mathematically, the SIRD model is represented by a system of ODEs: <span class="ellipsis">...</span> 
+            <span className="moreText">
+            The coefficients (β, γ, and α) are parameters that will be fit using the library covsirphy in Python. However, before we fit, we must recognise that our parameters are not gonna remain constant over time as quarantine measures changes, air traffic changes and so on. Therefore, we first split our data up into regions called “Phases”. 
+
+            A phase refers to sequential dates within which the parameters of our SIRD model are fixed. We can find these phases using S-R trend analysis with covsirphy. This technique comes from the mathematics behind SIR-derived models whereby the logarithm of S and R are directly proportional. Therefore, if we were to plot the logarithm of S on the y-axis and R on the x-axis, we should get a straight line. The slope of this line depends on the parameter values, therefore if we detect where the slope of this line changes, we can determine when in time our parameter values are changing. These breaking points are where we define our different phases. 
+
+            Now, with our phases in hand, we can optimize our model and get the best fit parameters over each different phase. For our vaccination simulation we only care about the most recent phase (as this is the phase after which we begin simulating) and therefore, we can save some time by not optimizing our parameters during the other phases. 
+
+            It should be noted that our model makes a few basic assumptions:
+            The parameter values do not change as we simulate the future (which is improbable), i.e. lockdown measures and etc. do not change during the time period.
+            Every case that exists is in the data set (i.e. no asymptomatic people who have not been tested).
+            We ignore time taken to distribute and assume all people get the vaccine immediately.
+            When simulating, we assume vaccine effectiveness does not depend on age and that there are no possible negative repercussions.
+            </span> <a class="more" href="#">read more</a></h3>
           </div>
           
            <div className="teacher-image-div">
